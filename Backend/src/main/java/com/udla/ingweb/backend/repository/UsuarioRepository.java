725ff4a25871b619f27b0a974e4a09b5f515f84e
Carlos Mejia <camh670@hotmail.com>
2022-10-03 11:48:00 -0500

    Add method get users

--- Backend/src/main/java/com/udla/ingweb/backend/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udla.ingweb.backend.repository;
-
-import com.udla.ingweb.backend.documents.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UsuarioRepository extends MongoRepository<Usuario,String> {
-}
