951755c8e788b9832b14cea524ee84c3c480858d
Carlos Mejia <camh670@gmail.com>
2022-12-19 19:38:45 -0500

    Fixed config to deploy back.

--- Backend/src/main/java/com/udla/ingweb/backend/BackendApplication.java
+++ Backend/src/main/java/com/udla/ingweb/backend/BackendApplication.java
@@ -25,7 +25,7 @@ public class BackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://frontend-core-angular-ingweb2022-2023.onrender.com")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
